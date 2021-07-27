e209b20445ae95b3e36eb1c6466be792980a5479
Alessandro Guggino <42747219+AlessandroGuggino@users.noreply.github.com>
2021-07-27 18:43:54 +0200

    Update README, Create library, Add comments

--- README.md
+++ README.md
@@ -1,14 +1,53 @@
-# Schorr Signatures
+# Schnorr Signatures
 
-This is a Schnorr signatures utility for educational purposes only.
+This is a **Schnorr signatures utility** for *educational purposes* only, developed by Fadi Barbara ([@disnocen](https://github.com/disnocen)) and published by BIT PoliTO.
+
+## Scripts
 
 There are four scripts:
 
-- `schnorr.py`: is the BIP340 reference implementation minimally changed to perform a test from the `test-vector.csv`; see <https://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki> and the reference implementation at <https://github.com/bitcoin/bips/tree/master/bip-0340>
-- `create_priv_key.py`: asks for a sentence (no newline characters), SAH256-hashes it and creates a key pair which can be use to `schnorr-sign` and `schnorr-verify` a message.
-- `schnorr-sign.py`: from a private key and a message, the scripts returns the signature and the public key; see `python3 schnorr-sign.py -h` for the syntax
-- `schnorr-verify.py`: from a public key, a message and a signature, the script returns `True` or `False`; see `python3 schnorr-verify.py -h` for the syntax
+- ##### Schnorr tester
+`schnorr.py`: is the BIP340 reference implementation minimally changed to perform a test from the `test-vector.csv`; see <https://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki> and the reference implementation at <https://github.com/bitcoin/bips/tree/master/bip-0340>.
+
+- ##### Private key creator
+`create_priv_key.py`: asks for a sentence (no newline characters), SAH256-hashes it and creates a key pair which can be use to `schnorr-sign` and `schnorr-verify` a message.
+
+- ##### Schnorr signer
+`schnorr-sign.py`: from a private key and a message, the scripts returns the signature and the public key; see `python3 schnorr-sign.py -h` for the syntax.
+
+- ##### Schnorr verifier
+`schnorr-verify.py`: from a public key, a message and a signature, the script returns `True` or `False`; see `python3 schnorr-verify.py -h` for the syntax.
 
 Both the scripts `schnorr-sign.py` and `schnorr-verify.py` are taken from the reference implementation. 
 
-I created the `create_priv_key.py`, but should not be used in production environments because it is not secure enough. 
+I created the `create_priv_key.py`, but should not be used in production environments because it is not secure enough.
+
+The functions used in those scripts are collected in the library `schnorrlib.py`.
+
+## How to install and run code
+### Installation
+
+```console
+# clone the repo
+$ git clone https://github.com/BITPoliTO/schnorr-sig.git
+
+# change the working directory to schnorr-sig
+$ cd schnorr-sig
+
+# install general requirements
+$ pip install typing 
+$ pip install hashlib
+```
+
+### Run
+```console
+$ python create_priv_key.py
+$ python schnorr-sign.py -s <private_key> -m <message>
+$ python schnorr-verify.py -s <signature> -p <public_key> -m <message>
+```
+
+#### Made to educate the BIT PoliTO team 🎓 by  
+  
+<a href="https://github.com/BITPoliTO/schnorr-sig/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=bitpolito/schnorr-sig" />
+</a>
---
+++ schnorrlib.py
@@ -0,0 +1,178 @@
+from typing import Tuple, Optional
+import hashlib
+
+# Elliptic curve parameters
+p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
+n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+G = (0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
+     0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B9)
+
+# Points are tuples of X and Y coordinates
+# the point at infinity is represented by the None keyword
+Point = Tuple[int, int]
+
+# Get bytes from an int
+def bytes_from_int(x: int) -> bytes:
+    return x.to_bytes(32, byteorder="big")
+
+# Get bytes from a point
+def bytes_from_point(P: Point) -> bytes:
+    return bytes_from_int(x(P))
+
+# Get an int from bytes
+def int_from_bytes(b: bytes) -> int:
+    return int.from_bytes(b, byteorder="big")
+
+# Get x coordinate from a point
+def x(P: Point) -> int:
+    return P[0]
+
+# Get y coordinate from a point
+def y(P: Point) -> int:
+    return P[1]
+
+# Point addition
+def point_add(P1: Optional[Point], P2: Optional[Point]) -> Optional[Point]:
+    if P1 is None:
+        return P2
+    if P2 is None:
+        return P1
+    if (x(P1) == x(P2)) and (y(P1) != y(P2)):
+        return None
+    if P1 == P2:
+        lam = (3 * x(P1) * x(P1) * pow(2 * y(P1), p - 2, p)) % p
+    else:
+        lam = ((y(P2) - y(P1)) * pow(x(P2) - x(P1), p - 2, p)) % p
+    x3 = (lam * lam - x(P1) - x(P2)) % p
+    return (x3, (lam * (x(P1) - x3) - y(P1)) % p)
+
+# Point multiplication
+def point_mul(P: Optional[Point], n: int) -> Optional[Point]:
+    R = None
+    for i in range(256):
+        if (n >> i) & 1:
+            R = point_add(R, P)
+        P = point_add(P, P)
+    return R
+
+# Note: 
+# This implementation can be sped up by storing the midstate
+# after hashing tag_hash instead of rehashing it all the time
+# Get the hash digest of (tag_hashed || tag_hashed || message)
+def tagged_hash(tag: str, msg: bytes) -> bytes:
+    tag_hash = hashlib.sha256(tag.encode()).digest()
+    return hashlib.sha256(tag_hash + tag_hash + msg).digest()
+
+# Check if a point is at infinity
+def is_infinity(P: Optional[Point]) -> bool:
+    return P is None
+
+# Get xor of bytes
+def xor_bytes(b0: bytes, b1: bytes) -> bytes:
+    return bytes(x ^ y for (x, y) in zip(b0, b1))
+
+# Get a point from bytes
+def lift_x_square_y(b: bytes) -> Optional[Point]:
+    x = int_from_bytes(b)
+    if x >= p:
+        return None
+    y_sq = (pow(x, 3, p) + 7) % p
+    y = pow(y_sq, (p + 1) // 4, p)
+    if pow(y, 2, p) != y_sq:
+        # ERRORE!
+        return None
+    return (x, y)
+
+def lift_x_even_y(b: bytes) -> Optional[Point]:
+    P = lift_x_square_y(b)
+    if P is None:
+        return None
+    else:
+        return (x(P), y(P) if y(P) % 2 == 0 else p - y(P))
+
+# Get hash digest with SHA256
+def hash_sha256(b: bytes) -> bytes:
+    return hashlib.sha256(b).digest()
+
+# Check if an int is square
+def is_square(x: int) -> bool:
+    return int(pow(x, (p - 1) // 2, p)) == 1
+
+# Check if a point has square y coordinate
+def has_square_y(P: Optional[Point]) -> bool:
+    infinity = is_infinity(P)
+    if infinity:
+        return False
+    assert P is not None
+    return is_square(y(P))
+
+# Check if a point has even y coordinate
+def has_even_y(P: Point) -> bool:
+    return y(P) % 2 == 0
+
+# Generate public key from an int
+def pubkey_gen_from_int(seckey: int) -> bytes:
+    P = point_mul(G, seckey)
+    assert P is not None
+    return bytes_from_point(P)
+
+# Generate public key from a hex
+def pubkey_gen_from_hex(seckey: hex) -> bytes:
+    seckey = bytes.fromhex(seckey)
+    d0 = int_from_bytes(seckey)
+    if not (1 <= d0 <= n - 1):
+        raise ValueError(
+            'The secret key must be an integer in the range 1..n-1.')
+    P = point_mul(G, d0)
+    assert P is not None
+    return bytes_from_point(P)
+
+# Generate Schnorr signature
+def schnorr_sign(msg: bytes, seckey: bytes, aux_rand: bytes) -> bytes:
+    if len(msg) != 32:
+        raise ValueError('The message must be a 32-byte array.')
+    d0 = int_from_bytes(seckey)
+    if not (1 <= d0 <= n - 1):
+        raise ValueError(
+            'The secret key must be an integer in the range 1..n-1.')
+    if len(aux_rand) != 32:
+        raise ValueError(
+            'aux_rand must be 32 bytes instead of %i.' % len(aux_rand))
+    P = point_mul(G, d0)
+    assert P is not None
+    d = d0 if has_even_y(P) else n - d0
+    t = xor_bytes(bytes_from_int(d), tagged_hash("BIP340/aux", aux_rand))
+    k0 = int_from_bytes(tagged_hash(
+        "BIP340/nonce", t + bytes_from_point(P) + msg)) % n
+    if k0 == 0:
+        raise RuntimeError(
+            'Failure. This happens only with negligible probability.')
+    R = point_mul(G, k0)
+    assert R is not None
+    k = n - k0 if not has_square_y(R) else k0
+    e = int_from_bytes(tagged_hash("BIP340/challenge",
+                                   bytes_from_point(R) + bytes_from_point(P) + msg)) % n
+    sig = bytes_from_point(R) + bytes_from_int((k + e * d) % n)
+    if not schnorr_verify(msg, bytes_from_point(P), sig):
+        raise RuntimeError('The created signature does not pass verification.')
+    return sig
+
+# Verify Schnorr signature
+def schnorr_verify(msg: bytes, pubkey: bytes, sig: bytes) -> bool:
+    if len(msg) != 32:
+        raise ValueError('The message must be a 32-byte array.')
+    if len(pubkey) != 32:
+        raise ValueError('The public key must be a 32-byte array.')
+    if len(sig) != 64:
+        raise ValueError('The signature must be a 64-byte array.')
+    P = lift_x_even_y(pubkey)
+    r = int_from_bytes(sig[0:32])
+    s = int_from_bytes(sig[32:64])
+    if (P is None) or (r >= p) or (s >= n):
+        return False
+    e = int_from_bytes(tagged_hash("BIP340/challenge",
+                                   sig[0:32] + pubkey + msg)) % n
+    R = point_add(point_mul(G, s), point_mul(P, n - e))
+    if (R is None) or (not has_square_y(R)) or (x(R) != r):
+        return False
+    return True
