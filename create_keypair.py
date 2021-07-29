81f8bb8335e20caef79f811cac4a696c886bae1c
disnocen <44157071+disnocen@users.noreply.github.com>
2021-07-29 07:51:30 +0200

    how to have more entropy for private key creation

--- create_keypair.py
+++ create_keypair.py
@@ -2,11 +2,16 @@ from schnorr_lib import n, pubkey_gen_from_int, hash_sha256
 
 
 if __name__ == "__main__":
+    
     string = str(input('Insert sentence as seed:\n'))
-
     dgst = hash_sha256(string.encode())
     dgst_hex = dgst.hex()
     privkey = (int(dgst_hex, 16) % n)
+    
+    # To have more entrpy you can do:
+    # import os
+    # priv = os.random(32)
+    # aint = (int(a, 16) % n)
 
     print("Your seed phrase is:", string)
     print()
