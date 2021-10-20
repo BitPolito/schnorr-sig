d35cc1618f14b69d4e9780ac0d79ae1a6c6b3a20
Luca Giorgino <lucas.10m.lg@gmail.com>
2021-10-20 11:49:14 +0200

    musig: correzione tipi (int,bytes, ...)

--- schnorr_musig.py
+++ schnorr_musig.py
@@ -7,6 +7,8 @@ from binascii import hexlify, unhexlify
 def main(argv):
 
     msg = "messaggio da firmare"
+    # msg_bytes = sl.hash_sha256(msg.encode()) # va effettuato l'hash? 
+    msg_bytes = msg.encode()
 
     # Get keypairs
     keypairs = json.load(open("keypairs.json", "r"))
@@ -20,21 +22,21 @@ def main(argv):
     Rsum = (0, 0)
     X = (0, 0)
 
-    for x,i in enumerate(keypairs["keypairs"]):
+    for i,x in enumerate(keypairs["keypairs"]):
         di = x["privateKey"]
         Pi = sl.pubkey_gen_from_hex(di)
         Psum = sl.point_add(Psum, Pi)
         
         # va bene generare k così? 
         t = sl.xor_bytes(unhexlify(di), sl.tagged_hash("BIP340/aux", sl.get_aux_rand()))
-        ki = sl.int_from_bytes(sl.tagged_hash("BIP340/nonce", t + sl.bytes_from_point(Pi) + msg)) % sl.n
+        ki = sl.int_from_bytes(sl.tagged_hash("BIP340/nonce", t + sl.bytes_from_point(Pi) + msg_bytes)) % sl.n
         if ki == 0:
             raise RuntimeError('Failure. This happens only with negligible probability.')
        
         keypairs["keypairs"][i]["ki"] = ki
         # print( keypairs["keypairs"][i]["ki"] = ki )
 
-        Ri = sl.point_mul(ki, sl.G)
+        Ri = sl.point_mul(sl.G, ki)
         Rsum = sl.point_add(Rsum, Ri)
 
         # bi = h(L||Pi), dove L = h(P1||..||Pn)
@@ -42,16 +44,17 @@ def main(argv):
         keypairs["keypairs"][i]["bi"] = bi
         # print( keypairs["keypairs"][i]["bi"] = bi )
 
-        xi = sl.point_mul(Pi, bi)
+        xi = sl.point_mul(Pi, sl.int_from_bytes(bi))
         X = sl.point_add(X, xi)
 
-    e_ = sl.hash_sha256(X + Rsum + msg)
+    e_ = sl.int_from_bytes(sl.hash_sha256(sl.bytes_from_point(X) + sl.bytes_from_point(Rsum) + msg_bytes))
     
     ssum = 0
     for x in keypairs["keypairs"]:
         # TODO dovremmo tornare in interi
-        di = x["privateKey"]
-        e = e_*x["bi"] % sl.n
+        di = sl.int_from_bytes(unhexlify(x["privateKey"]))
+        bi = sl.int_from_bytes(x["bi"])
+        e = e_* bi % sl.n
         si = x["ki"]+di+e
         ssum += si % sl.n
     
