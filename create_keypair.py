acd168e7017c2dce4e22eea75a4beec0814fbc7c
Alessandro Guggino <42747219+AlessandroGuggino@users.noreply.github.com>
2021-10-20 17:54:48 +0200

    Update create_keypair

--- create_keypair.py
+++ create_keypair.py
@@ -1,12 +1,44 @@
 from schnorr_lib import n, pubkey_gen_from_int
-import os
+import os, json, getopt, sys
 
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "hn:", ["n="])
+    except getopt.GetoptError:
+        print('[i] Command not found. Type -h for help')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('[i] Command: create_keypair.py -n <number_of_keys>')
+            sys.exit()
+        elif opt in ("-n", "--nkeys"):
+            n_keys = int(arg)
+
+    # Create json
+    keypairs = {
+        "$schema": "./keypairs_schema.json",
+        "keypairs": []
+    }
+
+    # Generate n keys
+    for i in range(0, n_keys):
+        priv = os.urandom(32)
+        privkey = int(priv.hex(), 16) % n
+
+        keypairs["keypairs"].append({
+            "privateKey": hex(privkey).replace('0x', ''),
+            "publicKey": pubkey_gen_from_int(privkey).hex()
+        })
+
+    json_object = json.dumps(keypairs, indent=4)
+    with open("keypairs.json", "w") as f:
+        f.write(json_object)
+    
+    print("[i] Keypair(s) generated")
+    
 
 if __name__ == "__main__":
     
-    priv = os.urandom(32)
-    privkey = int(priv.hex(), 16) % n
+    main(sys.argv[1:])
+
     
-    print("Your private key as hex is:", hex(privkey).replace('0x', ''))
-    print()
-    print("Your public key as hex is:", pubkey_gen_from_int(privkey).hex())
