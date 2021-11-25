a62f0f99321c3308026bc376275df6f2d9ad310c
SimonettaBodojra <simonetta.bodojra@studenti.polito.it>
2021-11-25 11:59:11 +0100

    create keypair + schnorr sign done jupyter

--- create_keypair.py
+++ create_keypair.py
@@ -3,14 +3,7 @@ import argparse, json, os
 from schnorr_lib import n, has_even_y, pubkey_point_gen_from_int, bytes_from_point 
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Creates one or more key pairs which are stored in a JSON file and can be used to sign and verify a message')
-    parser.add_argument('-n', '--nkeys', type=int, required=False, help='Number of pairs of keys to generate, if not specified a single keypair will be generated')
-    n_keys = parser.parse_args().nkeys
-
-    if not n_keys: 
-        n_keys = 1
-
+def create_keypair(n_keys: int):
     # Create json
     users = {
         "$schema": "./users_schema.json",
@@ -26,12 +19,24 @@ def main():
 
         # Check if the point P has the y-coordinate even; negate the private key otherwise
         privkey_even = privkey_int if has_even_y(publickey) else n - privkey_int
-        
+
         hex_privkey = hex(privkey_even).replace('0x', '').rjust(64, '0')
         users["users"].append({
             "privateKey": hex_privkey,
             "publicKey": bytes_from_point(publickey).hex()
         })
+    return users
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Creates one or more key pairs which are stored in a JSON file and can be used to sign and verify a message')
+    parser.add_argument('-n', '--nkeys', type=int, required=False, help='Number of pairs of keys to generate, if not specified a single keypair will be generated')
+    n_keys = parser.parse_args().nkeys
+
+    if not n_keys: 
+        n_keys = 1
+
+    users = create_keypair(n_keys)
 
     json_object = json.dumps(users, indent=4)
     with open("users.json", "w") as f:
